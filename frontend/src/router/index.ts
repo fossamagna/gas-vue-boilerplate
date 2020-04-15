56c557a7c3dbefbf12afc2982e7a40b3f1e39431
MURAKAMI Masahiko <m-murakami@esm.co.jp>
2020-04-15 11:34:44 +0900

    GASのWebアプリでもルーティングできるようにした
    - lazy-loadはできないのでやめた。
    - ルーティングのモードをhashにした
      GASのWebアプリのフロントはiframe内で動くので通常のborower historyを使えないから。

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
 
 Vue.use(VueRouter);
 
@@ -13,16 +14,12 @@ const routes: Array<RouteConfig> = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: About
   }
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: "hash",
   base: process.env.BASE_URL,
   routes
 });
