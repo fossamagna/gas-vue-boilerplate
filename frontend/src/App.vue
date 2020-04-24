75e1ea8f23219613b111a8ca81548f662fa8121a
MURAKAMI Masahiko <m-murakami@esm.co.jp>
2020-04-24 17:30:31 +0900

    Fix frontend lint error

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -45,14 +45,9 @@
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default Vue.extend({
   name: "App",
-
-  components: {
-    HelloWorld
-  },
 
   data: () => ({
     //
